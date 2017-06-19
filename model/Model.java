99d0b0aeacc9c3e926be419adb61466886b4eaf9
Flo <florian.pfeifer@viacesi.fr>
2017-06-19 09:22:29 +0200

    Add files via upload

---
+++ model/Model.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import model.dao.ExampleDAO;
+
+
+public class Model implements IModel {
+
+    
+    public Model() {
+       
+    }
+
+    
+   
+
+}
