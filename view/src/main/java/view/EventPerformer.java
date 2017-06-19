4f52be97502f8301537508c71a95499454fb4abb
VictorLosser <victor.losser@viacesi.fr>
2017-06-19 14:53:33 +0200

    Lecture touche phase 1

--- view/src/main/java/view/EventPerformer.java
+++ view/src/main/java/view/EventPerformer.java
@@ -3,9 +3,10 @@ package view;
 import java.awt.event.KeyEvent;
 
 import controller.IController;
+import controller.IUserOrder;
 
 
-public class EventPerformer {
+public class EventPerformer implements IEventPerformer{
 
 	private final IController orderPerformer;
 
@@ -13,8 +14,16 @@ public class EventPerformer {
 		this.orderPerformer = orderPerformer;
 	}
 	
-	private IController keyCodeToUserOrder(final int keyCode) {
-		IController userOrder;
+	@Override
+	public void eventPerform(final KeyEvent keyCode) {
+		final IUserOrder userOrder = this.keyCodeToUserOrder(keyCode.getKeyCode());
+		if (userOrder != null) {
+			this.orderPerformer.orderPerform(userOrder);
+		}
+	}
+	
+	private IUserOrder keyCodeToUserOrder(final int keyCode) {
+		IUserOrder userOrder;
 		switch (keyCode) {
 			case KeyEvent.VK_UP:
 				userOrder = new UserOrder(/*Order.UP*/);
