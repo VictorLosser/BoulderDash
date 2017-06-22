fece36c28aa645c75c0d34eeda57f0e2e1e257a7
Flenoir <Flenoir@StarGuardian>
2017-06-22 16:52:51 +0200

    test commit git hub
    Signed-off-by: Flenoir <Flenoir@StarGuardian>

--- model/src/main/java/object/Mobile.java
+++ model/src/main/java/object/Mobile.java
@@ -1,5 +1,70 @@
 package object;
 
 public class Mobile {
+	
+/*Java est le nom de marque d'une technique informatique développée initialement par Sun Microsystems puis par Oracle : la « technologie Java™ ».
 
-}
+Java est utilisé dans une grande variété de plates-formes depuis les systèmes embarqués et les téléphones mobiles, les ordinateurs individuels, les serveurs, les applications d’entreprise, les superordinateurs, etc.
+
+Défini à l'origine comme un langage, « Java » a évolué au cours du temps pour devenir un ensemble cohérent d'éléments techniques et non techniques. Ainsi, la technologie Java regroupe :
+
+    Des standards (la plate-forme Java) définis sous forme de spécification par le Java Community Process (JCP), en trois éditions :
+        Java SE (standard edition),
+        Java EE (enterprise edition), s'appuyant sur Java SE,
+        Java ME (micro edition), indépendante des deux précédentes ;
+    Des logiciels (langages informatiques, bibliothèques, frameworks, serveurs d'application, outils d'aide au développement), dont :
+        Des implémentations (concurrentes) de ces spécifications,
+        Un écosystème d'autres logiciels s'appuyant sur tout ou partie de ces standards, voire leur faisant concurrence ;
+    Des communautés d'entreprises, organisations à but non lucratif (fondations, Java User Groups, universités) et indépendants, membres ou non du JCP, possédant tout ou partie des marques, brevets, parts de marché liés à la technologie Java.
+
+Java est un des termes les plus connus du monde de l'informatique et de l'Internet, que ce soit des professionnels comme du grand public. Sa notoriété est telle que Sun, avant d'être acheté par Oracle Corporation, a décidé de l'utiliser pour son symbole boursier au Nasdaq, symbole qui était SUNW à l'origine, et est devenu JAVA1.
+
+Sommaire
+
+    1 Histoire
+        1.1 Langage Java
+        1.2 Internet
+        1.3 L'ouverture
+        1.4 Historique des versions de Java SE
+    2 Standard Java
+        2.1 Java Community Process
+        2.2 Spécifications
+            2.2.1 Java SE
+            2.2.2 Java EE
+            2.2.3 Java ME
+    3 Logiciels
+        3.1 Selon l'usage et le vecteur d'utilisation
+        3.2 Langages de programmation
+    4 Communautés
+    5 Références
+    6 Annexes
+        6.1 Articles connexes
+        6.2 Liens externes
+
+Histoire
+Langage Java
+
+Le langage Java a débuté dans les années 1990 avec James Gosling qui souhaitait développer un langage de programmation indépendant de la plate-forme hardware. Oak (traduction : « Chêne ») fut un échec.
+Logo de Java représentant une tasse de café.
+
+Par la suite, Bill Joy (cofondateur de la firme Sun Microsystems) proposa une nouvelle version de Oak appelée « Java ». Son but était de pallier une déficience des langages de programmation en produisant un langage conçu pour des machines et des logiciels hétérogènes.
+
+On trouve gratuitement sur le marché une machine virtuelle (Java Virtual Machine), un compilateur, ainsi que de nombreux outils visant à faciliter l'investissement du Web par Java.
+
+Après de très nombreuses modifications visant à améliorer le système, Java est devenu plus qu’une simple solution Internet, c’est dorénavant un langage utilisé pour toutes sortes de développements, distribués, client lourd ou léger, etc.
+Internet
+
+Java naquit en même temps que le Web, et toutes les qualités que requiert le Web étaient implémentées dans Java :
+
+    un langage permettant un fonctionnement sur des machines structurées différemment ;
+    une nécessité de bande passante la plus minime possible, pour pallier l’étroitesse de celle offerte par le Web.
+
+Un navigateur Java apparut dès 1994 (HotJava) qui, tout en intégrant Java, était capable de faire fonctionner les applets Java (applications Java exécutées par le navigateur).
+
+Netscape fut l’un des éléments essentiels dans la création et l’implantation de Java dans le parc informatique, en intégrant Java dans son logiciel.
+
+L'idée de départ a été de réduire le travail du serveur ainsi que la quantité de données transmises via le réseau, en confiant davantage de tâches à chaque ordinateur client.
+L'ouverture
+Sun a annoncé le 13 novembre 2006 que l’environnement Java serait sous licence libre GNU GPL (avec extension « ClassPath ») par étapes : immédiatement pour le code source du compilateur Java ainsi que pour celui de la machine virtuelle Java HotSpot2. En mai 2007, lors de la conférence JavaOne, c’est l’ensemble du code Java qui a été mis en open source (à l’exception de quelques rares parties fournies en format binaire pour l’instant encore). L’exception « ClassPath » permet d’utiliser cette JVM GPL sans pour autant que les applications soient elles aussi nécessairement sous licence GPL. Quelque temps auparavant Sun avait déjà mis en Open Source son serveur d’application Java EE 5 complet GlassFish.
+*/
+	}
