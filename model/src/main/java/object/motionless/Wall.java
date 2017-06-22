6cabdece0978e5c1e49da6493259c244dad69a70
Flenoir <Flenoir@StarGuardian>
2017-06-22 10:49:56 +0200

    test
    Signed-off-by: Flenoir <Flenoir@StarGuardian>

--- model/src/main/java/object/motionless/Wall.java
+++ model/src/main/java/object/motionless/Wall.java
@@ -4,14 +4,5 @@ import object.Motionless;
 
 public class Wall extends Motionless{
 
-	//private Image imgWall;
-	//private ImageIcon icoWall;
-
-	public Wall(int x, int y){
-		//super(x, y, 0, 0);
-		//this.icoWall = new ImageIcon(getClass().getResource("/image/Wall.png"));
-		//this.imgWall = this.icoWall.getImage();
-	}
-	//public Image getImgWall() {return imgWall;}
 
 }
