8f08796dcc956b1f45dd676b4f857a9613f310f8
VictorLosser <victor.losser@viacesi.fr>
2017-06-22 10:17:08 +0200

    Package states (GameState + MenuState + State)

---
+++ view/src/main/java/states/GameState.java
@@ -0,0 +1,29 @@
+package states;
+
+import java.awt.Graphics;
+
+public class GameState extends State {
+
+	private Player player;
+	private World world;
+	
+	public GameState(Game game){
+		super(game);
+		player = new Player(game, 100,100);
+		world = new World("res/world/world1.txt");
+	}
+	
+	
+	@Override
+	public void tick() {
+		world.tick();
+		player.tick();
+	}
+
+	@Override
+	public void render(Graphics g) {
+		world.render(g);
+		player.render(g);
+	}
+
+}
